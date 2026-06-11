2546a9840599e3ebe671d6f9d3553df7800151df
diegoreinoso4051-svg <diego.reinoso4051@gmail.com>
2026-06-11 14:47:32 -0500

    ciclo do while

--- src/whilepractica/Whilepractica.java
+++ src/whilepractica/Whilepractica.java
@@ -4,6 +4,8 @@
  */
 package whilepractica;
 
+import java.util.Scanner;
+
 /**
  *
  * @author diegoreinoso
@@ -14,9 +16,31 @@ public class Whilepractica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // TODO code application logic here
         System.out.println("hola mundo");
-        System.out.println("practica con github y ciclo while");
+        System.out.println("practica con github y ciclo hile");
+        int opcion;
+        do {
+            System.out.println("1.suma");
+            System.out.println("2.resta");
+            System.out.println("ingrese una opcion");
+            
+            opcion = sc.nextInt();
+            
+            switch (opcion){
+                case 1:
+                    System.out.println("funcion suma");
+                    break;
+                case 2:
+                    System.out.println("funcion resta");
+                    break;
+                default:
+                    System.out.println("opcion incorrecta");
+                    
+            }       
+            
+        } while (opcion <=3);
     }
     
 }
